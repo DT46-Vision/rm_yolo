98e7fbc5330ea8d862ce8368e6cac592bf2e3d61
dbinkv1 <dbinkv1@Gmail.com>
2024-08-13 21:44:56 +0800

    refactor(rm_yolo_aim): 重构YOLO对象检测逻辑并启动连续检测
    - 重构predit.py中的代码结构，定义YOLOObjectDetector类以封装YOLO模型的初始化、对象检测和串口通信。
    - 实现find_available_cameras方法以查找可用摄像头索引。
    - 在start_detection方法中启动连续的对象检测，显示检测结果并处理退出条件。
    - 更新.gitignore文件以包含log目录，有助于在版本控制中忽略日志文件。
    - 在predit_node.py中创建YOLOObjectDetector实例，准备在ROS2节点中使用。
    - 通过这些修改，提高了代码的模块化和可维护性，同时实现了在ROS2环境中的对象检测功能。

--- rm_yolo_aim/rm_yolo_aim/predit.py
+++ rm_yolo_aim/rm_yolo_aim/predit.py
@@ -1,118 +1,102 @@
 from ultralytics import YOLO
 import cv2
 from loguru import logger
-
 import serial
 
-com = serial.Serial('/dev/ttyUSB0', 115200) 
+class YOLOObjectDetector:
+    def __init__(self, model_path, serial_port='/dev/ttyUSB0', baud_rate=115200):
+        self.model = YOLO(model_path)
+        self.com = serial.Serial(serial_port, baud_rate)
+        self.logger = logger
+        self.target_class = 'B1'
 
-com.write(f'0,0,[{90},{90}]'.encode('ascii'))
+        # 打印所有类别名称和它们的索引
+        for idx, name in self.model.names.items():
+            self.logger.info(f'类别索引: {idx}, 名称: {name}')
 
-# filter = AdaptiveFilter(0)
+    def get_class_index(self, class_name): # 根据类别名称返回类别索引
+        return next((idx for idx, name in self.model.names.items() if name == class_name), None)
 
-logger.info('加载模型...')
+    def predict(self, img):
+        results = self.model(img)
+        self.logger.info(f'用时 {results[0].speed}')
+        annotated_img = results[0].plot()
+        img_height, img_width = img.shape[:2]
+        # self.logger.info(f'图像尺寸 {img_height} x {img_width}')
 
-# 加载训练后的模型
-model = YOLO('models/best.pt')
-# model = YOLO('models/yolov8n.pt')
+        target_idx = self.get_class_index(self.target_class)
 
-# 打印所有类别名称和它们的索引
-for idx, name in model.names.items():
-    logger.info(f'类别索引: {idx}, 名称: {name}')
+        if target_idx is None:
+            self.logger.warning(f'未找到类别 "{self.target_class}"')
+            return annotated_img, None
 
-def get_class_index(class_name):
-    """根据类别名称返回类别索引"""
-    return next((idx for idx, name in model.names.items() if name == class_name), None)
+        boxes = results[0].boxes
+        max_area = 0
+        max_center = None
 
-def capture_image(cam=0):
-    cap = cv2.VideoCapture(cam)
-    ret, img = cap.read()
-    logger.info(f'Captured image {ret}')
-    cv2.imwrite('output/captured_image.jpg', img)
-    cap.release()
-    return img
+        for box in boxes:
+            if box.cls == target_idx:
+                x1, y1, x2, y2 = box.xyxy[0]
+                area = (x2 - x1) * (y2 - y1)
 
+                if area > max_area:
+                    max_area = area
+                    max_center = ((x1 + x2) / 2, (y1 + y2) / 2)
 
-def predict(img, target_class='person'):
-    results = model(img)
-    logger.info(f'用时 {results[0].speed}')
-    annotated_img = results[0].plot()
-    # 获取图像尺寸
-    img_height, img_width = img.shape[:2]
-    logger.info(f'图像尺寸 {img_height} x {img_width}')
+        if max_center is not None:
+            pixel_x = int(max_center[0].item())
+            pixel_y = int(max_center[1].item())
 
-    # 获取目标类别的索引
-    target_idx = get_class_index(target_class)
+            # self.logger.info(f"pixel_x: {pixel_x}, pixel_y: {pixel_y}")
 
-    if target_idx is None:
-        logger.warning(f'未找到类别 "{target_class}"')
-        return annotated_img, None
+            servo_x = pixel_x - int(img_width  / 2)
+            servo_y = pixel_y - int(img_height / 2)
 
-    boxes = results[0].boxes
-    max_area = 0
-    max_center = None
+            uart_msg = f'[{servo_x},{servo_y}]'
 
-    for box in boxes:
-        if box.cls == target_idx:
-            x1, y1, x2, y2 = box.xyxy[0]
-            area = (x2 - x1) * (y2 - y1)
+            self.com.write(uart_msg.encode('ascii'))
+            self.logger.info(f'发送到串口的数据: {uart_msg}')
 
-            if area > max_area:
-                max_area = area
-                max_center = ((x1 + x2) / 2, (y1 + y2) / 2)
-
-    if max_center is not None:
+            cv2.circle(annotated_img, (int(pixel_x), int(pixel_y)), 5, (0, 0, 255), -1)  # 目标中心点
+            cv2.circle(annotated_img, (int(img_width / 2), int(img_height / 2)), 2, (0, 255, 0), -1)# 准星
             
-        pixel_x = int(max_center[0].item())
-        pixel_y = int(max_center[1].item())
+            # self.logger.info(f'最大面积目标的中心坐标: {max_center}')
+        else:
+            self.logger.info('未检测到目标')
 
-        logger.info(f"pixel_x: {pixel_x}, pixel_y: {pixel_y}")
+        return annotated_img, max_center
 
-        servo_x = ((pixel_x/img_width)-0.5) * (75/2) * 0.3
-        servo_y = ((pixel_y/img_height)-0.5) * (75/2) * 0.1
+    @staticmethod
+    def find_available_cameras(start_index=0, end_index=9):
+        available_cameras = []
+        for index in range(start_index, end_index + 1):
+            cap = cv2.VideoCapture(index)
+            if cap.isOpened():
+                available_cameras.append(index)
+                cap.release()
+        return available_cameras
 
-        com.write(f'[{servo_x},{0},0,0]'.encode('ascii'))
-        logger.info(f'发送到串口的数据: {servo_x}, {servo_y}')
+    def start_detection(self, camera_index=2):
+        self.logger.info(f'可用摄像头：{self.find_available_cameras()}')
+        self.logger.info('开始预测')
 
-        cv2.circle(annotated_img, (int(pixel_x),int(pixel_y)), 5, (0, 0, 255), -1)
-        logger.info(f'最大面积目标的 中心坐标: {max_center}')
-    else:
-        logger.info('未检测到 目标')
+        cap = cv2.VideoCapture(camera_index)
 
-    return annotated_img, max_center
+        if cap is not None:
+            while True:
+                ret, frame = cap.read()
+                if ret:
+                    PD_frame, max_center = self.predict(frame)
 
+                    cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
+                    cv2.imshow("frame", PD_frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
 
-def find_available_cameras(start_index=0, end_index=9):
-    available_cameras = []
-    for index in range(start_index, end_index + 1):
-        cap = cv2.VideoCapture(index)
-        if cap.isOpened():
-            available_cameras.append(index)
+        cv2.destroyAllWindows()
+        if cap is not None:
             cap.release()
-    return available_cameras
-
 
 if __name__ == '__main__':
-    logger.info(f'可用摄像头：{find_available_cameras()}')
-
-    logger.info('开始预测')
-
-    cap = cv2.VideoCapture(2)
-
-    if cap is not None:
-        while True:
-            ret, frame = cap.read()
-            if ret:
-                #PD_frame, max_center = predict(frame, target_class='cell phone')  
-                PD_frame, max_center = predict(frame, target_class='B1')  # 这里可以更改目标类别
-
-                logger.info(f'坐标：{max_center}')
-
-                cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-                cv2.imshow("frame", PD_frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-
-    cv2.destroyAllWindows()
-    if cap is not None:
-        cap.release()
+    detector = YOLOObjectDetector('/home/dbink/ros_ws/src/rm_yolo_aim/rm_yolo_aim/models/best.pt')
+    detector.start_detection()
