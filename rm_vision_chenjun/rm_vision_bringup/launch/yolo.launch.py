6db0549d437063a459d57b5920042107d6b92982
dbink <dbinkv1@Gmail.com>
2024-08-26 17:40:25 +0800

    refactor(misc): 代码风格和结构优化
    代码改动包括：
    - 统一引号使用：将所有单引号替换为双引号，用于字符串字面量。
    - 增加空行：在某些模块的导入之间以及函数定义之前增加空行，以提高可读性。
    - 调整缩进：将某些代码块的缩进从制表符转换为4个空格，以保持一致性。
    - 简化返回：在yolo.launch.py中简化了generate_launch_description函数的返回语句。
    
    这些调整符合PEP 8编码标准，使代码风格更加一致和整洁。

--- rm_vision_chenjun/rm_vision_bringup/launch/yolo.launch.py
+++ rm_vision_chenjun/rm_vision_bringup/launch/yolo.launch.py
@@ -1,5 +1,4 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
@@ -9,29 +8,38 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     params_file = os.path.join(
-        get_package_share_directory('mindvision_camera'), 'config', 'camera_params.yaml')
+        get_package_share_directory("mindvision_camera"), "config", "camera_params.yaml"
+    )
 
-    camera_info_url = 'package://mindvision_camera/config/camera_info.yaml'
+    camera_info_url = "package://mindvision_camera/config/camera_info.yaml"
 
-    return LaunchDescription([
-        Node(                                  # 配置一个节点的启动
-            package='rm_yolo_aim',             # 节点所在的功能包
-            executable='predit_node',          # 节点的可执行文件
-        ),
-        DeclareLaunchArgument(name='params_file',
-                              default_value=params_file),
-        DeclareLaunchArgument(name='camera_info_url',
-                              default_value=camera_info_url),
-        DeclareLaunchArgument(name='use_sensor_data_qos',
-                              default_value='false'),
-        Node(
-            package='mindvision_camera',
-            executable='mindvision_camera_node',
-            output='screen',
-            emulate_tty=True,
-            parameters=[LaunchConfiguration('params_file'), {
-                'camera_info_url': LaunchConfiguration('camera_info_url'),
-                'use_sensor_data_qos': LaunchConfiguration('use_sensor_data_qos'),
-            }],
-        )
-    ])
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(name="params_file", default_value=params_file),
+            DeclareLaunchArgument(
+                name="camera_info_url", default_value=camera_info_url
+            ),
+            DeclareLaunchArgument(name="use_sensor_data_qos", default_value="false"),
+            Node(
+                package="rm_yolo_aim",
+                executable="predit_node",
+                output="screen",
+                emulate_tty=True,
+            ),
+            Node(
+                package="mindvision_camera",
+                executable="mindvision_camera_node",
+                output="screen",
+                emulate_tty=True,
+                parameters=[
+                    LaunchConfiguration("params_file"),
+                    {
+                        "camera_info_url": LaunchConfiguration("camera_info_url"),
+                        "use_sensor_data_qos": LaunchConfiguration(
+                            "use_sensor_data_qos"
+                        ),
+                    },
+                ],
+            ),
+        ]
+    )
