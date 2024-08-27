676b5ba1577ecb7f4b18201b83454aa9bce2d183
dbink <dbinkv1@Gmail.com>
2024-08-27 15:03:31 +0800

    refactor(predit_node): 更新导入和检测调用以符合ArmorDetector
    将YOLO目标检测器替换为Armor检测器，并在predit_node.py中进行相应的更改。更新了导入语句和检测方法的调用，以适配新的ArmorDetector类。此外，调整了发布者对象的声明，以提高代码的可读性。

--- rm_yolo_aim/rm_yolo_aim/predit_node.py
+++ rm_yolo_aim/rm_yolo_aim/predit_node.py
@@ -14,22 +14,22 @@ from cv_bridge import CvBridge          # ROS与OpenCV图像转换类
 import cv2                              # Opencv图像处理库
 import numpy as np                      # Python数值计算库
 
-from rm_yolo_aim.predit import YOLOObjectDetector
+from rm_yolo_aim.predit import ArmorDetector
 
-detector = YOLOObjectDetector('/home/morefine/ros_ws/src/rm_yolo_aim/rm_yolo_aim/models/best.pt')
+detector = ArmorDetector('/home/morefine/ros_ws/src/rm_yolo_aim/rm_yolo_aim/models/best.pt')
 
 class ImageSubscriber(Node):
     def __init__(self, name):
         super().__init__(name)                                # ROS2节点父类初始化
         self.sub = self.create_subscription(
             Image, 'image_raw', self.listener_callback, 10)   # 创建订阅者对象
-        self.publisher_img = self.create_publisher(Image, '/detector/result_img', 10)  # 创建图像发布者
-        self.publisher_uart = self.create_publisher(String, '/uart_msg_send', 10)     # 创建串口信息发布者
+        self.publisher_img  = self.create_publisher(Image, '/detector/result_img', 10)  # 创建图像发布者
+        self.publisher_uart = self.create_publisher(String, '/uart_msg_send', 10)       # 创建串口信息发布者
         self.cv_bridge = CvBridge()                           # 创建图像转换对象
 
     def listener_callback(self, data):
         image = self.cv_bridge.imgmsg_to_cv2(data, 'bgr8')    # 将ROS的图像消息转化成OpenCV图像
-        img, uart_msg = detector.predict(image)               # 检测图像
+        img, uart_msg = detector.detect_armor(image)               # 检测图像
 
         # 发布处理后的图像
         result_img_msg = self.cv_bridge.cv2_to_imgmsg(img, 'bgr8')
@@ -43,7 +43,7 @@ class ImageSubscriber(Node):
 
 def main(args=None):                            # ROS2节点主入口main函数
     rclpy.init(args=args)                       # ROS2 Python接口初始化
-    node = ImageSubscriber("predit_node")  # 创建ROS2节点对象
+    node = ImageSubscriber("predit_node")       # 创建ROS2节点对象
     rclpy.spin(node)                            # 循环等待ROS2退出
     node.destroy_node()                         # 销毁节点对象
     rclpy.shutdown()                            # 关闭ROS2 Python接口
