0587ada582c007014de035d0b2784ee4427dd032
dbink <dbinkv1@Gmail.com>
2024-08-31 13:40:04 +0800

    新功能：支持检测和发送装甲板信息的JSON字符串
    实现了一个新的消息类型`ArmorsMsg`，用于封装检测到的装甲板信息，并通过ROS2进行发布。该消息类型包含一个`Header`和一个JSON字符串，其中JSON字符串包含了关于检测到的装甲板的详细信息。之前的字符串消息类型已替换为这种新的、更结构化的消息类型，以提高信息传递的效率和可扩展性。
    
    此改动要求更新`rm_interfaces`和`rm_yolo_aim`包，包括修改`CMakeLists.txt`和`package.xml`文件，以支持新的消息依赖关系，并调整`armor_detector_node.py`以使用`ArmorsMsg`类型进行数据发布

--- rm_yolo_aim/rm_yolo_aim/armor_detector_node.py
+++ rm_yolo_aim/rm_yolo_aim/armor_detector_node.py
@@ -1,14 +1,14 @@
 import rclpy                            # ROS2 Python接口库
 from rclpy.node import Node             # ROS2 节点类
 from sensor_msgs.msg import Image       # 图像消息类型
-from std_msgs.msg import String         # 字符串消息类型
+from std_msgs.msg import Header         # 头部消息类型
 from cv_bridge import CvBridge          # ROS与OpenCV图像转换类
-# import cv2                              # OpenCV图像处理库
-# import numpy as np                      # Python数值计算库
+import cv2                              # OpenCV图像处理库
+import numpy as np                      # Python数值计算库
 import json                             # JSON序列化库
 
 from rm_yolo_aim.armor_detector import ArmorDetector
-# from rm_interfaces.msg import ArmorList, ArmorInfo  # 导入自定义消息类型
+from rm_interfaces.msg import ArmorsMsg  # 导入自定义消息类型
 
 # detector = ArmorDetector('/home/morefine/ros_ws/src/rm_yolo_aim/rm_yolo_aim/models/best.pt')             # pt 原始模型
 detector = ArmorDetector('/home/morefine/ros_ws/src/rm_yolo_aim/rm_yolo_aim/models/best_openvino_model/')  # openvino 模型
@@ -19,7 +19,7 @@ class ImageSubscriber(Node):
         self.sub = self.create_subscription(
             Image, 'image_raw', self.listener_callback, 10)   # 创建订阅者对象
         self.publisher_img  = self.create_publisher(Image, '/detector/result_img', 10)  # 创建图像发布者
-        self.publisher_armors = self.create_publisher(String, '/uart_msg_send', 10)       # 创建串口信息发布者
+        self.publisher_armors = self.create_publisher(ArmorsMsg, '/uart_msg_send', 10)  # 创建串口信息发布者
         self.cv_bridge = CvBridge()                           # 创建图像转换对象
 
     def listener_callback(self, data):
@@ -34,9 +34,16 @@ class ImageSubscriber(Node):
         # 将装甲板信息字典转换为JSON格式的字符串
         armors_json = json.dumps(armors_dict)
 
-        # 发布JSON格式的装甲板信息
-        armors_msg = String()
+        # 创建自定义消息对象并添加Header
+        armors_msg = ArmorsMsg()
+        armors_msg.header = Header()  # 创建并设置Header
+        armors_msg.header.stamp = self.get_clock().now().to_msg()  # 设置时间戳
+        armors_msg.header.frame_id = 'armor_frame'  # 可根据需要设置frame_id
+
+        # 设置JSON格式的装甲板信息
         armors_msg.data = armors_json
+
+        # 发布消息
         self.publisher_armors.publish(armors_msg)
         self.get_logger().info(f'Published UART message: {armors_msg.data}')
 
