fac6b9c1612a22ccb49482e6248998c380cc907a
dbink <dbinkv1@Gmail.com>
2024-08-31 13:14:28 +0800

    feat✨: refactor(armor_detector_node): 优化图像处理和信息发布流程
    在图像订阅者节点中，已更新检测装甲的图像处理库注释，并实现将装甲信息以JSON格式发布。不再直接发布字典类型，而是将其序列化为JSON字符串，封装在String消息中进行发布。这一改变提高了信息发布的灵活性和兼容性。
    
    此外，优化了主函数main的代码结构，确保ROS2初始化和关闭过程的清晰执行。

--- rm_yolo_aim/rm_yolo_aim/armor_detector_node.py
+++ rm_yolo_aim/rm_yolo_aim/armor_detector_node.py
@@ -3,11 +3,12 @@ from rclpy.node import Node             # ROS2 节点类
 from sensor_msgs.msg import Image       # 图像消息类型
 from std_msgs.msg import String         # 字符串消息类型
 from cv_bridge import CvBridge          # ROS与OpenCV图像转换类
-import cv2                              # Opencv图像处理库
-import numpy as np                      # Python数值计算库
+# import cv2                              # OpenCV图像处理库
+# import numpy as np                      # Python数值计算库
+import json                             # JSON序列化库
 
 from rm_yolo_aim.armor_detector import ArmorDetector
-from rm_interfaces.msg import ArmorList, ArmorInfo  # 导入自定义消息类型
+# from rm_interfaces.msg import ArmorList, ArmorInfo  # 导入自定义消息类型
 
 # detector = ArmorDetector('/home/morefine/ros_ws/src/rm_yolo_aim/rm_yolo_aim/models/best.pt')             # pt 原始模型
 detector = ArmorDetector('/home/morefine/ros_ws/src/rm_yolo_aim/rm_yolo_aim/models/best_openvino_model/')  # openvino 模型
@@ -23,16 +24,21 @@ class ImageSubscriber(Node):
 
     def listener_callback(self, data):
         image = self.cv_bridge.imgmsg_to_cv2(data, 'bgr8')    # 将ROS的图像消息转化成OpenCV图像
-        img, armors_dict = detector.detect_armor(image)        # 检测图像
+        img, armors_dict = detector.detect_armor(image)       # 检测图像，返回处理后的图像和装甲板信息字典
 
         # 发布处理后的图像
         result_img_msg = self.cv_bridge.cv2_to_imgmsg(img, 'bgr8')
         self.publisher_img.publish(result_img_msg)
         self.get_logger().info('Published processed image to /detector/result_img')
 
-        # 发布串口信息
-        self.publisher_armors.publish(armors_dict)
-        self.get_logger().info(f'Published UART message: {armors_dict}')
+        # 将装甲板信息字典转换为JSON格式的字符串
+        armors_json = json.dumps(armors_dict)
+
+        # 发布JSON格式的装甲板信息
+        armors_msg = String()
+        armors_msg.data = armors_json
+        self.publisher_armors.publish(armors_msg)
+        self.get_logger().info(f'Published UART message: {armors_msg.data}')
 
 def main(args=None):                            # ROS2节点主入口main函数
     rclpy.init(args=args)                       # ROS2 Python接口初始化
