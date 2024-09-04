56ec5a2191516437e754d63106592b6759031068
DBinK <DBinKv1@Gmail.com>
2024-09-04 23:10:50 +0800

    refactor(start.sh stop.sh): 简化ROS工作空间设置和停止脚本
    在[start.sh](file://start.sh)中，ROS工作空间的source路径已更新为使用波浪号(~)表示用户主目录，以简化路径引用。
    
    在[stop.sh](file://stop.sh)中，kill命令的顺序已调整，先杀掉启动脚本和ROS进程，再执行ros2 daemon的停止和启动操作。这一改变确保所有ROS相关进程被适当处理，避免潜在的进程残留问题。
    
    此外，[stop.sh](file://stop.sh)现在使用`ros2 node killall`来杀死

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source /opt/ros/humble/setup.bash
-source /home/morefine/ros_ws/install/setup.bash
+source ~/ros_ws/install/setup.bash
 
 ros2 launch rm_vision_bringup yolo.launch.py
--- stop.sh
+++ stop.sh
@@ -5,9 +5,11 @@ export ROS_HOME=${ROS_HOME:=$HOME_DIR/.ros}
 export ROS_LOG_DIR="/tmp"
 
 source /opt/ros/humble/setup.bash
-pkill start.sh # 杀掉看门狗
-pkill -f ros  # 杀掉所有ROS2进程
+
+ros2 node killall
+
+pkill start.sh # 杀掉启动文件
+pkill -f ros   # 杀掉所有ROS2进程
 ros2 daemon stop
 ros2 daemon start
 sleep 1
-
