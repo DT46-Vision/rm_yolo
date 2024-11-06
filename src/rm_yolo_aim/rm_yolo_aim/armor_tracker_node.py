b5f94de7d861cfc7c1a0f3f0e26d472972a712a2
“Rihoko” <a1219814581@163.com>
2024-11-06 12:32:39 +0800

    update

--- src/rm_yolo_aim/rm_yolo_aim/armor_tracker_node.py
+++ src/rm_yolo_aim/rm_yolo_aim/armor_tracker_node.py
@@ -1,12 +1,63 @@
 import json                             # JSON序列化库
-
 import rclpy                            # ROS2 Python接口库
 from rclpy.node import Node             # ROS2 节点类
 from std_msgs.msg import String, Header # 字符串消息类型和头部消息类型
-
 from sensor_msgs.msg import Image       # 图像消息类型
 from rm_interfaces.msg import ArmorsMsg, ArmorTracking  # 导入自定义消息类型
 from rm_yolo_aim.armor_tracker import select_tracking_armor, pixel_to_angle_and_deep
+import cv2
+import numpy as np
+global yaw_last, pitch_last
+yaw_last = None
+pitch_last = None
+
+class KalmanFilter(object):
+
+    def __init__(self):
+
+        self.kalman = cv2.KalmanFilter(4, 2) # 4：状态数，包括（x，y，dx，dy）坐标及速度（每次移动的距离）；2：观测量，能看到的是坐标值
+        self.kalman.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32) # 系统测量矩阵
+        self.kalman.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]],      np.float32) # 状态转移矩阵
+        self.kalman.processNoiseCov = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)*0.03 # 系统过程噪声协方差
+        self.current_measurement = np.array((2, 1), np.float32)
+        self.last_measurement = np.array((2, 1), np.float32)
+        self.current_prediction = np.zeros((2, 1), np.float32)
+        self.last_prediction = np.zeros((2, 1), np.float32)
+        self.error_frame = 0
+
+    def track(self,x,y):
+        self.last_prediction = self.current_prediction # 把当前预测存储为上一次预测
+        self.last_measurement = self.current_measurement # 把当前测量存储为上一次测量
+
+        if abs(self.last_measurement[0] - x) > 64 or abs(self.last_measurement[1] - y) > 48: 
+                self.error_frame = self.error_frame + 1
+        else :
+                pass
+
+        if x ==0 and y == 0 : 
+                self.error_frame = self.error_frame + 1
+        else :
+                pass
+
+        if self.error_frame < 5 and self.error_frame > 0  :
+            self.current_measurement = np.array([[np.float32(self.last_prediction[0])], [np.float32(self.last_prediction[1])]])
+
+        else:
+            self.current_measurement = np.array([[np.float32(x)], [np.float32(y)]]) # 当前测量
+            self.error_frame = 0
+
+        print("error:",self.error_frame)
+        self.kalman.correct(self.current_measurement) # 用当前测量来校正卡尔曼滤波器
+        self.current_prediction = self.kalman.predict() # 计算卡尔曼预测值，作为当前预测
+
+        lmx, lmy = self.last_measurement[0], self.last_measurement[1] # 上一次测量坐标
+        cmx, cmy = self.current_measurement[0], self.current_measurement[1] # 当前测量坐标
+        lpx, lpy = self.last_prediction[0], self.last_prediction[1] # 上一次预测坐标
+        cpx, cpy = self.current_prediction[0], self.current_prediction[1] # 当前预测坐标
+
+       
+        return cpx,cpy
+
 
 class ArmorTrackerNode(Node):
     def __init__(self, name):
@@ -26,6 +77,8 @@ class ArmorTrackerNode(Node):
         self.tracking_color = 1    # 0蓝色表示, 1表示红色, 现初始化为红色
         self.tracking_armor = None
         self.pic_width = 666       # 随便初始化一个图像宽度
+        self.kalmanfilter = KalmanFilter()
+
 
     def listener_callback_cam(self, data):
         if self.pic_width != data.width:
@@ -43,8 +96,9 @@ class ArmorTrackerNode(Node):
             self.get_logger().info(f"得到需要 追踪 的装甲板 {self.tracking_armor}")
 
             yaw, pitch, deep = pixel_to_angle_and_deep(self.tracking_armor, 72, self.pic_width) 
-
-            self.get_logger().info(f"yaw, pitch, deep: {yaw, pitch, deep}")
+            print("yaw,????? pitch:", yaw, pitch)
+            yaw_predict, pitch_predict = self.kalmanfilter.track(yaw, pitch)
+            self.get_logger().info(f"yaw, pitch, deep: {yaw_predict, pitch_predict, deep}")
             
             # 将装甲板信息字典转换为msg消息定义的格式
             tracking_armor_json = json.dumps(self.tracking_armor)
@@ -57,8 +111,8 @@ class ArmorTrackerNode(Node):
 
             # 设置的装甲板信息
             tracking_armor_msg.data  = tracking_armor_json
-            tracking_armor_msg.yaw   = float(yaw)
-            tracking_armor_msg.pitch = float(pitch)
+            tracking_armor_msg.yaw   = float(yaw_predict)
+            tracking_armor_msg.pitch = float(pitch_predict)
             tracking_armor_msg.deep  = float(deep)
 
             # 发布消息
@@ -80,7 +134,7 @@ class ArmorTrackerNode(Node):
             self.tracking_color = msg.tracking_color
 
 def main(args=None):                              # ROS2节点主入口main函数
-    rclpy.init(args=args)                         # ROS2 Python接口初始化
+    rclpy.init(args=args)      # ROS2 Python接口初始化               
     node = ArmorTrackerNode("armor_tracker_node")     # 创建ROS2节点对象
     rclpy.spin(node)                              # 循环等待ROS2退出
     node.destroy_node()                           # 销毁节点对象
