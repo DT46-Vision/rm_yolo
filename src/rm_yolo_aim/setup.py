1ede012f71c1c072be80771c63e9f03a22633f0a
“Rihoko” <a1219814581@163.com>
2024-11-23 12:42:04 +0800

    fix setup bug

--- src/rm_yolo_aim/setup.py
+++ src/rm_yolo_aim/setup.py
@@ -22,7 +22,7 @@ setup(
         'console_scripts': [
             'armor_tracker_node = rm_yolo_aim.armor_tracker_node:main',
             'armor_detector_opencv_node = rm_yolo_aim.armor_detector_opencv_node:main',
-            'armor_detector_ai_node = rm_yolo_aim.armor_detector_ai_node:main',
+            'armor_detector_ai_node = rm_yolo_aim.armor_detector_node:main',
         ],
     },
 )
