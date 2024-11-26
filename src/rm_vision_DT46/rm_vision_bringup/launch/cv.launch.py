5faefcf7a50d08c6c78f91a06143c8d8e6b03fbb
“Rihoko” <a1219814581@163.com>
2024-11-26 12:09:50 +0800

    update

--- src/rm_vision_DT46/rm_vision_bringup/launch/cv.launch.py
+++ src/rm_vision_DT46/rm_vision_bringup/launch/cv.launch.py
@@ -29,10 +29,10 @@ def generate_launch_description():
             emulate_tty=True,
         ),        
 
-        # 启动 armor_detector_node
+        # 启动 armor_detector_cv_node
         Node(
             package="rm_yolo_aim",
-            executable="armor_detector_node",
+            executable="armor_detector_cv_node",
             output="screen",
             emulate_tty=True,
         ),
