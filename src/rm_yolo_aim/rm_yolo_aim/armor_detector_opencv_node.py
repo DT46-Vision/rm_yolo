ece267b7a448fedb128aecf5552d9c5f490f849a
DBinK <DBinKv1@Gmail.com>
2025-03-15 19:15:21 +0800

    perf(rm_yolo_aim): 调整二值化阈值以优化装甲板识别
    - 将 binary_val 参数从 40 修改为 14
    - 此修改旨在提高装甲板识别的准确性和鲁棒性

--- src/rm_yolo_aim/rm_yolo_aim/armor_detector_opencv_node.py
+++ src/rm_yolo_aim/rm_yolo_aim/armor_detector_opencv_node.py
@@ -16,7 +16,7 @@ detect_color =  2  # 颜色参数 0: 识别红色装甲板, 1: 识别蓝色装�
 display_mode = 0 # 显示模式 None: 不显示, Binary: 显示二值化图, All: 显示二值化图和结果图像
 
 # 图像参数字典
-binary_val = 40  
+binary_val = 14  
 light_params = {
     "light_area_min": 5,  # 最小灯条面积
     "light_angle_min": -45,  # 最小灯条角度
