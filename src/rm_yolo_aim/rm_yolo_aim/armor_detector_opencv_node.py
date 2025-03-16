a01e2339c1ba67b2ea57b733a286cdb1a8923480
“Rihoko” <a1219814581@163.com>
2025-03-16 20:39:16 +0800

    feat(rm_yolo_aim): 添加灯条颜色识别比例参数
    - 在 armor_detector_opencv.py 和 armor_detector_opencv_node.py 中添加了 light_red_ratio 和 light_blue_ratio 参数
    - 修改了颜色识别逻辑，增加了比例参数以提高识别准确性
    - 这些参数允许调整红色和蓝色灯条识别的灵敏度，从而适应不同光照条件下的颜色变化

--- src/rm_yolo_aim/rm_yolo_aim/armor_detector_opencv_node.py
+++ src/rm_yolo_aim/rm_yolo_aim/armor_detector_opencv_node.py
@@ -21,6 +21,8 @@ light_params = {
     "light_area_min": 5,  # 最小灯条面积
     "light_angle_min": -45,  # 最小灯条角度
     "light_angle_max": 45,  # 最大灯条角度
+    "light_red_ratio": 1.0,
+    "light_blue_ratio": 1.0,
     "light_angle_tol": 8,  # 灯条角度容差
     "vertical_discretization": 0.8,  # 垂直离散
     "height_tol": 15,  # 高度容差
