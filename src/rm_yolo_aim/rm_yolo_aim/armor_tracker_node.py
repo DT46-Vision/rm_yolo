53da68691b247138fac20336d79e318f7f39dee3
dbink <dbinkv1@Gmail.com>
2025-03-17 20:50:20 +0800

    update offset

--- src/rm_yolo_aim/rm_yolo_aim/armor_tracker_node.py
+++ src/rm_yolo_aim/rm_yolo_aim/armor_tracker_node.py
@@ -51,6 +51,7 @@ class ArmorTrackerNode(Node):
         self.reflection_hight_tol = 20
         self.reflection_cx_tol = 10
         self.offset_yaw = 2.5
+        self.offset_pitch = 0.0
 
         self.pub_tracker = self.create_publisher(ArmorTracking, '/tracker/target', 10) # 创建发布者/tracker/target
 
@@ -65,6 +66,7 @@ class ArmorTrackerNode(Node):
         self.declare_parameter('reflection_hight_tol', self.reflection_hight_tol)  # 声明 frame_add 参数
         self.declare_parameter('reflection_cx_tol', self.reflection_cx_tol)  # 声明 detect_color 参数
         self.declare_parameter('offset_yaw', self.offset_yaw)  # 声明 detect_color 参数
+        self.declare_parameter('offset_pitch', self.offset_pitch)  # 声明 detect_color 参数
         self.add_on_set_parameters_callback(self.param_callback)  # 添加参数回调
         self.get_logger().info('Armor Tracker Node has started.')
 
@@ -84,6 +86,8 @@ class ArmorTrackerNode(Node):
                 self.reflection_cx_tol = param.value
             if param.name == 'offset_yaw':
                 self.offset_yaw = param.value
+            if param.name == 'offset_pitch':
+                self.offset_pitch = param.value
         return SetParametersResult(successful=True)  # 返回成功结果
 
     def listener_callback_cam(self, data):
@@ -161,8 +165,8 @@ class ArmorTrackerNode(Node):
 
             # 设置的装甲板信息
             tracking_armor_msg.data  = tracking_armor_json
-            tracking_armor_msg.yaw   = float(yaw + )
-            tracking_armor_msg.pitch = float(pitch)
+            tracking_armor_msg.yaw   = float(yaw + self.offset_yaw)
+            tracking_armor_msg.pitch = float(pitch + self.offset_pitch)
             tracking_armor_msg.deep  = float(deep)
 
             self.get_logger().info(f"发布的 tracking_armor_msg: {tracking_armor_msg}")
