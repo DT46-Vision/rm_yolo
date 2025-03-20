c27160cea34513b503a3eeffc467c4e7dda7882b
DBinK <DBinKv1@Gmail.com>
2025-03-20 17:02:03 +0800

    perf(camera): 调整曝光时间和二值化阈值
    - 将 camera_params.yaml 和 camera_params_infantry.yaml 中的曝光时间从 200us 调整为 30us
    - 调整 armor_detector_opencv_node.py 中的二值化阈值和灯条比例参数

--- src/rm_yolo_aim/rm_yolo_aim/armor_detector_opencv_node.py
+++ src/rm_yolo_aim/rm_yolo_aim/armor_detector_opencv_node.py
@@ -15,14 +15,14 @@ import time
 detect_color =  2  # 颜色参数 0: 识别红色装甲板, 1: 识别蓝色装甲板, 2: 识别全部装甲板
 display_mode = 0 # 显示模式 None: 不显示, Binary: 显示二值化图, All: 显示二值化图和结果图像
 
-binary_val_blue = 23
-binary_val_red = 22
+binary_val_blue = 26
+binary_val_red = 25
 light_params = {
     "light_area_min": 5,  # 最小灯条面积
     "light_angle_min": -35,  # 最小灯条角度
     "light_angle_max": 35,  # 最大灯条角度
-    "light_red_ratio": 2.0,
-    "light_blue_ratio": 1.2,
+    "light_red_ratio": 3.5,
+    "light_blue_ratio": 3.5,
     "light_angle_tol": 7,  # 灯条角度容差
     "vertical_discretization": 1.5,  # 垂直离散
     "height_tol": 18,  # 高度容差
